c3cb84d5df2e739a1b637618031dd8ce140991b2
Chamika Dimantha <chamika.dimantha@westpay.se>
2024-03-27 21:46:44 +0100

    Start desinging the welcome, the register and the login pages

--- app/src/main/java/com/example/balancebeacon_fe/Components/LoginPage.java
+++ app/src/main/java/com/example/balancebeacon_fe/Components/LoginPage.java
@@ -1,4 +1,4 @@
-package com.example.balancebeacon_fe;
+package com.example.balancebeacon_fe.Components;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.balancebeacon_fe.R;
 
 public class LoginPage extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/balancebeacon_fe/Components/WelcomePage.java
@@ -0,0 +1,66 @@
+package com.example.balancebeacon_fe.Components;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.balancebeacon_fe.R;
+
+public class WelcomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_welcome_page);
+
+        ImageView welcomeLoginButton;
+        ImageView welcomeRegisterButton;
+
+        welcomeLoginButton = findViewById(R.id.welcome_login_button);
+        welcomeRegisterButton = findViewById(R.id.welcome_register_button);
+
+
+        /**
+         * this function works when clicking on the Login button in the Welcome page
+         * when clicking it navigates to the Login page
+         */
+        welcomeLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WelcomePage.this, LoginPage.class);
+                startActivity(intent);
+            }
+        });
+
+        /**
+         * this function works when clicking on the Register button in the Welcome page
+         * when clicking it navigates to the Register page
+         */
+        welcomeRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WelcomePage.this, RegisterPage.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+//    {
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+//    };
+    }
+}
